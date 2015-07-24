aae0d7fd009adb2bf2c5ecdf7ca5c657e5833164
John Jenkins <jenkins@mcs.anl.gov>
2015-07-24 08:35:02 -0500

    update tests to stress OOM

--- tests/test-simple.sh
+++ tests/test-simple.sh
@@ -12,23 +12,23 @@ rm -rf "$out_dir-ser" "$out_dir-ser-big" "$out_dir-opt-big"
 ./tests/test-simple --codes-config="${srcdir}"/tests/conf/test-simple.conf --lp-io-dir="$out_dir"-ser
 
 set -e
-grep "write_bytes:50000" "$out_dir"-ser/lsm-category-all >/dev/null
-grep "read_bytes:50000" "$out_dir"-ser/lsm-category-all >/dev/null
+grep "write_bytes:300000" "$out_dir"-ser/lsm-category-all >/dev/null
+grep "read_bytes:300000" "$out_dir"-ser/lsm-category-all >/dev/null
 set +e
 
 ./tests/test-simple --codes-config="${srcdir}"/tests/conf/test-simple-big.conf --lp-io-dir="$out_dir"-ser-big
 
 set -e
-grep "write_bytes:500000" "$out_dir"-ser-big/lsm-category-all >/dev/null
-grep "read_bytes:500000" "$out_dir"-ser-big/lsm-category-all >/dev/null
+grep "write_bytes:3000000" "$out_dir"-ser-big/lsm-category-all >/dev/null
+grep "read_bytes:3000000" "$out_dir"-ser-big/lsm-category-all >/dev/null
 set +e
 
 # TODO
 mpirun -np 4 ./tests/test-simple --sync=3 --codes-config="${srcdir}"/tests/conf/test-simple-big.conf --lp-io-dir="$out_dir"-opt-big
 
 set -e
-grep "write_bytes:500000" "$out_dir"-opt-big/lsm-category-all >/dev/null
-grep "read_bytes:500000" "$out_dir"-opt-big/lsm-category-all >/dev/null
+grep "write_bytes:3000000" "$out_dir"-opt-big/lsm-category-all >/dev/null
+grep "read_bytes:3000000" "$out_dir"-opt-big/lsm-category-all >/dev/null
 set +e
 
 rm -rf "$out_dir-ser" "$out_dir-ser-big" "$out_dir-opt-big"
