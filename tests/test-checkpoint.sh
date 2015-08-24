e7e38b287f4efb08bd4b0c53750b829cdbc7e9e0
mubarak <mubarak@mcs.anl.gov>
2015-08-24 15:58:28 -0500

    fixing test checkpoint compilation

--- tests/test-checkpoint.sh
+++ tests/test-checkpoint.sh
@@ -11,7 +11,6 @@ rm -rf "$out_dir-ser" "$out_dir-ser-big" "$out_dir-opt-big"
 
 #removing the data matching checks because we don't know how much data is going to be sent/received in this case
 
-./tests/test-checkpoint --workload_type = checkpoint_io_workload --codes-config="${srcdir}"/tests/conf/test-simple.conf --lp-io-dir="$out_dir"-ser
-./tests/test-checkpoint --workload_type = checkpoint_io_workload --codes-config="${srcdir}"/tests/conf/test-simple-big.conf --lp-io-dir="$out_dir"-ser-big
-mpirun -np 4 ./tests/test-checkpoint --sync=3 --codes-config="${srcdir}"/tests/conf/test-simple-big.conf --lp-io-dir="$out_dir"-opt-big
-./tests/test-simple --workload_type = checkpoint_io_workload --sync=3 --codes-config="${srcdir}"/tests/conf/test-dragonfly.conf --lp-io-dir="$out_dir"-dfly-opt
+
+./tests/test-checkpoint --sync=1 --codes-conf="${srcdir}"/tests/conf/test-checkpoint.conf --lp-io-dir="$out_dir"-ser
+mpirun -np 4 ./tests/test-checkpoint --sync=3 --codes-conf="${srcdir}"/tests/conf/test-checkpoint.conf --lp-io-dir="$out_dir"-ser
